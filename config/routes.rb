972b569e4169c5e8206109386b722cb6b5023a59
shan tirmizi <tirmizishahnawaz@gmail.com>
2020-11-16 17:32:21 +0000

    Finised creating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books, only: [ :index, :new, :show, :create, :update, :destroy, :edit ] do 
+    resources :bookings, only: [ :new, :create ]
+  end
+
+  resources :users, only: [:show]
 end
