38f1d70ea7b29f6baf6e355913669cf543b9671f
shan tirmizi <tirmizishahnawaz@gmail.com>
2020-11-17 12:03:40 +0000

    finised the show, create, new and authencate login

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,13 @@
 class Book < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :genre, presence: true
+  validates :condition, presence: true
+  validates :year, presence: true
+  validates :publisher, presence: true
+
+
+
 end
