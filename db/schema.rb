eca87d61307d70c24cdc325e2606cb0a87e0eaaf
Giovanni Reniero <giovanni.reniero@gmail.com>
2020-11-17 11:34:20 +0000

    removed price from bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_160244) do
+ActiveRecord::Schema.define(version: 2020_11_17_111907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2020_11_16_160244) do
   create_table "bookings", force: :cascade do |t|
     t.string "start_date"
     t.string "end_date"
-    t.integer "price"
     t.boolean "accept_decline"
     t.bigint "book_id", null: false
     t.bigint "user_id", null: false
