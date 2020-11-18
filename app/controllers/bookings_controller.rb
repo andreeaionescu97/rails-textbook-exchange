9ad0658ee46d5e1cf6a9c1251404e949d8b2a69a
Giovanni Reniero <giovanni.reniero@gmail.com>
2020-11-18 19:04:45 +0000

    added Geomap with markers final

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,31 +1,28 @@
 class BookingsController < ApplicationController
-  def new
-    @booking = Booking.new
-    @book = Book.find(params[:book_id])
-  end
 
-  def create
-    @booking = Booking.new(booking_params)
-    @book = Book.find(params[:book_id])
-    @booking.user = current_user
-    @booking.book = @book
-    if @booking.save!
-      redirect_to booking_path(@booking)
-    else
-      render :new
+    def new
+        @booking = Booking.new
+        @book = Book.find(params[:book_id])
     end
-  end
 
-  def show
-    @booking = Booking.find(params[:id])
-    @book = @booking.book
-  end
+    def create
+        @booking = Booking.new(booking_params)
+        @book = Book.find(params[:book_id])
+        @booking.user = current_user
+        @booking.book = @book
+        if @booking.save!
+            redirect_to book_path(@book)
+        else
+            render :new
+        end
+    end
 
-  private
+   
 
-  def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :price)
-  end
+    private
 
+    def booking_params
+        params.require(:booking).permit(:start_date, :end_date, :price)
+    end
 
 end
