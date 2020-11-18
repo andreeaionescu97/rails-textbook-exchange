fe2673716b65e57aa3c6a24265626b752fec6ad5
Giovanni Reniero <giovanni.reniero@gmail.com>
2020-11-18 15:09:29 +0000

    added controller and html on index for map

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,35 +1,42 @@
 class BooksController < ApplicationController
-  def index
-    @books = Book.all
-  end
+    def index
+      @books = Book.all
+      @markers = @books.geocoded.map do |book|
+        {
+          lat: flat.latitude,
+          lng: flat.longitude
+        }
+      end
+    end
+  
 
-  def show
-    @book = Book.find(params[:id])
-  end
+    def show
+      @book = Book.find(params[:id])
+    end
 
-  def new
-    @book = Book.new
-  end
+    def new
+      @book = Book.new
+    end
 
-  def create
-    @book = Book.new(book_params)
-      @book.user = current_user
-        if @book.save!
-          redirect_to book_path(@book)
-        else
-          render :new
-        end
-  end
+    def create
+      @book = Book.new(book_params)
+        @book.user = current_user
+          if @book.save!
+            redirect_to book_path(@book)
+          else
+            render :new
+          end
+    end
 
-  def destroy
-    @book = Book.find(params[:id])
-    @book.destroy
-    redirect_to books_path
-  end
+    def destroy
+      @book = Book.find(params[:id])
+      @book.destroy
+      redirect_to books_path
+    end
 
-  private
+    private
 
-  def book_params
-    params.require(:book).permit(:title, :author, :genre, :condition, :year, :publisher, :photo)
-  end
+    def book_params
+      params.require(:book).permit(:title, :author, :genre, :condition, :year, :publisher, :photo, :address)
+    end
 end
