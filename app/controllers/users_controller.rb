fffeedb25d8de6ea06310326272a41d05f475aca
shan tirmizi <tirmizishahnawaz@gmail.com>
2020-11-18 14:55:38 +0000

    finised user profile

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+
+    def show
+        @user = current_user
+        #how do i create upload just an image avatar
+        # redirect_to user_path(@user)
+    end
+    # we need to only create the photo for which the user already exist
+    
+end
